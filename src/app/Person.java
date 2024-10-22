82aef79779d3ac6b001e80f051367bfc26bc9e42
anna-masl <anna.maslinnikova@gmail.com>
2024-10-22 16:39:31 +0300

    first solution

---
+++ src/app/Person.java
@@ -0,0 +1,24 @@
+package app;
+
+public class Person {
+
+    public String name;
+    public int age;
+    public String profession;
+
+    public Person(String name, int age, String profession) {
+        this.name = name;
+        this.age = age;
+        this.profession = profession;
+    }
+
+    @Override
+    public String toString() {
+        return "Ім'я: " + name  +
+                ", Вік: " + age +
+                ", Професія: " + profession ;
+    }
+
+
+
+}
---
+++ src/app/SolutionHomeWork11.java
@@ -0,0 +1,26 @@
+package app;
+
+public class SolutionHomeWork11 {
+    public static void main(String[] args) {
+
+        Person john = new Person("John",30,"Інженер");
+        Person mary = new Person("Mary",25,"Вчитель");
+        Person bob = new Person("Bob",35,"Лікар");
+
+
+        System.out.println("1. " + john);
+        System.out.println("   "+ mary);
+        System.out.println("   "+ bob);
+        System.out.println(" ");
+        System.out.println(" ");
+
+        Person alice = new Person("Alice",28, "Архітектор");
+        System.out.println("2. " + alice);
+        System.out.println("   (Після оновлення професії)");
+        alice.profession = "Дизайнер";
+        System.out.println("   "+ alice);
+
+
+
+    }
+}
